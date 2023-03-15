8eebd8b291699a80f8a9f490cdddad87e6cbb606
m-a-t-h-a-i <mathaimarvin574@gmail.com>
2023-03-15 12:57:27 +0300

    Creating custom validations

--- app/models/production.rb
+++ app/models/production.rb
@@ -4,6 +4,19 @@ class Production < ApplicationRecord
     # To validate our data we use the validate built in method
     validates :title, presence: true
 
+    # Creating custom validations
+    # WE use validate not validates
+
+    validate :no_more_musicals
+
+    def no_more_musicals
+        #   We use an if statement
+        if genre.downcase == "musical"
+            # We add our own custom error message to the errors method for the genre only
+            errors.add(:genre, "Sorry no more musicals!")
+        end
+    end
+
     def title_director
         "#{title}: #{director}"
     end
